7fbfd90c90b695a97487df3bbea9c49ce7c6762f
Amauri Pietropaolo <amauripietropaolo@gmail.com>
2021-06-14 12:40:55 -0300

    esqueleto

---
+++ Projeto2/script.js
@@ -0,0 +1,14 @@
+// let esconder = document.getElementById('esconder');
+//     opt1 = document.querySelector('body');
+//     opt2 = document.querySelector('body');
+function hide(){
+    document.getElementById('esconder').hidden = true;
+}
+function show(){
+    document.getElementById('esconder').hidden = false;
+}
+
+var input3 = document.getElementById();
+function gerenciaDivs(){
+    if()
+}
